b2511cdeea662b38f8eba029dab0206c0e148919
Tarra Ghaffari <tarrag2525@gmail.com>
2022-12-08 19:30:27 +0000

    Added a function to retrieve carsfrom the database.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import HelloWorld from '../components/hello-world';
 
-export default function Home(props) {
-  return (
-    <div>
-      <HelloWorld />
-    </div>
-  );
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inventory: null
+    };
+    this.getCars = this.getCars.bind(this);
+  }
+
+  getCars() {
+    fetch('api/getCars')
+      .then(res => res.json())
+      .then(cars => console.log(cars));
+  }
+
+  componentDidMount() {
+    this.getCars();
+  }
+
+  render() {
+
+    if (!this.state.inventory) return null;
+
+  }
 }
